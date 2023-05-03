807ac4f039d046308af52debd735db8f64224198
Sreza <130074829+Rezaabdollahi7@users.noreply.github.com>
2023-05-03 20:26:14 +0330

    add banners & mobile offers

--- accets/js/main.js
+++ accets/js/main.js
@@ -1,48 +1,57 @@
-
- 
 var countDownDate = new Date("may 1, 2024 15:37:25").getTime();
 
 var x = setInterval(function () {
-    var now = new Date().getTime();
-    var distance = countDownDate - now ;
-    var days = Math.floor(distance /(1000*60*60*60));
-    var hours = Math.floor(distance %(1000*60*60*24)/(1000*60*60));
-    var minutes = Math.floor(distance %(1000*60*60)/(1000*60));
-    var seconds = Math.floor(distance %(1000*60)/(1000));
-    document.getElementById("demo").innerHTML =
-     '<span id="day">' + days + '</span> ' + '<span id="hours">' + hours + '</span> ' + '<span id="minutes">' + minutes + '</span>' + '<span id="seconds">' + seconds + '</span>';
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 60));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById("demo").innerHTML =
+    '<span id="day">' +
+    days +
+    "</span> " +
+    '<span id="hours">' +
+    hours +
+    "</span> " +
+    '<span id="minutes">' +
+    minutes +
+    "</span>" +
+    '<span id="seconds">' +
+    seconds +
+    "</span>";
 
-     var z = document.getElementsByClassName("demos");
-     for(i=0 ; i<z.length ; i++){
-        z[i].innerHTML =
-          '<span id="day">' +
-          days +
-          "</span> " +
-          '<span id="hours">' +
-          hours +
-          "</span> " +
-          '<span id="minutes">' +
-          minutes +
-          "</span>" +
-          '<span id="seconds">' +
-          seconds +
-          "</span>";
-     }
-    if(distance<0){
-        clearInterval(x);
-        document.getElementById("demo").innerHTML = "";
-        
-        document.getElementById("after-expire").setAttribute("id","offer-expire-text");
-        document.getElementById("offer-expire-text").innerHTML = "Time Out";
-        document.getElementById("offer-expire-text").style.background ="rgba(176, 175, 175, 0.3)";
-        document.getElementById("offer-blur").style.filter = "blur(2px)";
-    }     
-},1000);
+  var z = document.getElementsByClassName("demos");
+  for (i = 0; i < z.length; i++) {
+    z[i].innerHTML =
+      '<span id="day">' +
+      days +
+      "</span> " +
+      '<span id="hours">' +
+      hours +
+      "</span> " +
+      '<span id="minutes">' +
+      minutes +
+      "</span>" +
+      '<span id="seconds">' +
+      seconds +
+      "</span>";
+  }
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "";
 
+    document
+      .getElementById("after-expire")
+      .setAttribute("id", "offer-expire-text");
+    document.getElementById("offer-expire-text").innerHTML = "Time Out";
+    document.getElementById("offer-expire-text").style.background =
+      "rgba(176, 175, 175, 0.3)";
+    document.getElementById("offer-blur").style.filter = "blur(2px)";
+  }
+}, 1000);
 
-$(".carousel").carousel();
-    $(document).ready(function () {
-      $(".owl-carousel").owlCarousel();
-    });
-
-
+// $(".carousel").carousel();
+//     $(document).ready(function () {
+//       $(".owl-carousel").owlCarousel();
+//     });
