7609e9668ccba631080cc1ca9e6e92380ffb9614
Sreza <130074829+Rezaabdollahi7@users.noreply.github.com>
2023-05-08 23:49:52 +0330

    end of product details in product pages

--- accets/js/main.js
+++ accets/js/main.js
@@ -170,61 +170,8 @@ typewriter
                 .pauseFor(2500)
                 .start();
 
-//---------------------------- Modals ------------------------------
-// Get the modal
-// var modal = document.getElementById("myModal");
+//----------------------------Shipping Modals ------------------------------
 
-// Get the button that opens the modal
-// var btn = document.getElementById("myBtn");
-// var btn_2 = document.getElementById("myBtn_2");
-// var btn_3 = document.getElementById("myBtn_3");
-
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
-
-// // When the user clicks on the button, open the modal
-// btn.onclick = function() {
-//   modal.style.display = "block";
-// }
-// btn_2.onclick = function () {
-//   modal.style.display = "block";
-// };
-// btn_3.onclick = function () {
-//   modal.style.display = "block";
-// };
-
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//   modal.style.display = "none";
-// }
-
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// }  
-
-//chatgpt
-// const modal = document.getElementById("myModal");
-// const btns = document.querySelectorAll("#myBtn, #myBtn_2, #myBtn_3");
-// const span = document.getElementsByClassName("close")[0];
-
-// btns.forEach((btn) => {
-//   btn.onclick = function () {
-//     modal.style.display = "block";
-//   };
-// });
-
-// span.onclick = function () {
-//   modal.style.display = "none";
-// };
-
-// window.onclick = function (event) {
-//   if (event.target === modal) {
-//     modal.style.display = "none";
-//   }
-// };
 // Get all the modal buttons and modals
 var modalBtns = document.querySelectorAll('.open-modal-btn');
 var modals = document.querySelectorAll('.modal');
@@ -260,4 +207,29 @@ window.addEventListener('click', function(event) {
     event.target.style.display = 'none';
   }
 });
+
+// ------------------- quantity in products page ------------------------
+// Get the plus and minus <a> tags and the input element
+var plusBtn = document.getElementById("plus");
+var minusBtn = document.getElementById("minus");
+var numberInput = document.getElementById("numberInput");
+
+// Add event listener to plus button
+plusBtn.addEventListener("click", function(event) {
+  // Prevent the default behavior of the link
+  event.preventDefault();
+  // Increment the input value
+  numberInput.value++;
+});
+
+// Add event listener to minus button
+minusBtn.addEventListener("click", function(event) {
+  // Prevent the default behavior of the link
+  event.preventDefault();
+  // Decrement the input value if it's greater than 0
+  if (numberInput.value > 0) {
+    numberInput.value--;
+  }
+});
+
 
